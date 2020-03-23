087ac93b8590463e2789ce10d927f9b6786a558e
davidbyoung <dbyoung2@gmail.com>
2020-03-23 18:21:33 -0500

    Renamed App to Application for consistency, moved exception renderers to Framework because they're more of a framework integration than part of the Exceptions library, created MissingConfigurationValueException and InvalidConfigurationFileException to replace the singular ConfigurationException (more specific exceptions), removed concept of "exception results" for the console in favor of output writers that can write output and return a status code, added the ability to add console output writers to the ExceptionHandlerComponent

---
+++ tests/Exceptions/Console/ConsoleExceptionRendererTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Aphiria
+ *
+ * @link      https://www.aphiria.com
+ * @copyright Copyright (C) 2020 David Young
+ * @license   https://github.com/aphiria/aphiria/blob/master/LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace Aphiria\Framework\Tests\Exceptions\Console;
+
+use Aphiria\Console\Output\IOutput;
+use Aphiria\Framework\Exceptions\Console\ConsoleExceptionRenderer;
+use Exception;
+use InvalidArgumentException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests the console exception renderer
+ */
+class ConsoleExceptionRendererTest extends TestCase
+{
+    private ConsoleExceptionRenderer $exceptionRenderer;
+    /** @var IOutput|MockObject */
+    private IOutput $output;
+
+    protected function setUp(): void
+    {
+        $this->output = $this->createMock(IOutput::class);
+        $this->exceptionRenderer = new ConsoleExceptionRenderer($this->output, false);
+    }
+
+    public function testRenderingExceptionWithManyRegisteredOutputWritersWritesMessagesAndReturnsStatusCodes(): void
+    {
+        $this->exceptionRenderer->registerManyOutputWriters([
+            Exception::class => function (Exception $ex, IOutput $output) {
+                $output->writeln('foo');
+
+                return 0;
+            },
+            InvalidArgumentException::class => function (InvalidArgumentException $ex, IOutput $output) {
+                $output->writeln('bar');
+
+                return 1;
+            }
+        ]);
+        $this->output->expects($this->at(0))
+            ->method('writeln')
+            ->with('foo');
+        $this->output->expects($this->at(1))
+            ->method('writeln')
+            ->with('bar');
+        $this->exceptionRenderer->render(new Exception);
+        $this->exceptionRenderer->render(new InvalidArgumentException);
+    }
+
+    public function testRenderingExceptionWithNoRegisteredOutputWriterUsesDefaultResultMessage(): void
+    {
+        $exception = new Exception();
+        $this->output->expects($this->once())
+            ->method('writeln')
+            ->with(["<fatal>{$exception->getMessage()}" . \PHP_EOL . "{$exception->getTraceAsString()}</fatal>"]);
+        $this->exceptionRenderer->render($exception);
+    }
+
+    public function testRenderingExceptionWithRegisteredOutputWriterUsesIt(): void
+    {
+        $this->exceptionRenderer->registerOutputWriter(
+            Exception::class,
+            function (Exception $ex, IOutput $output) {
+                $output->writeln('foo');
+
+                return 1;
+            }
+        );
+        $this->output->expects($this->once())
+            ->method('writeln')
+            ->with('foo');
+        $this->exceptionRenderer->render(new Exception);
+    }
+
+    public function testSettingOutputUsesNewOutputToWriteExceptionMessages(): void
+    {
+        $newOutput = $this->createMock(IOutput::class);
+        $newOutput->expects($this->once())
+            ->method('writeln')
+            ->with('foo');
+        $this->exceptionRenderer->setOutput($newOutput);
+        $this->exceptionRenderer->registerOutputWriter(
+            Exception::class,
+            function (Exception $ex, IOutput $output) {
+                $output->writeln('foo');
+
+                return 0;
+            }
+        );
+        $this->exceptionRenderer->render(new Exception);
+    }
+}
---
+++ tests/Exceptions/Http/HttpExceptionRendererTest.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * Aphiria
+ *
+ * @link      https://www.aphiria.com
+ * @copyright Copyright (C) 2020 David Young
+ * @license   https://github.com/aphiria/aphiria/blob/master/LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace Aphiria\Framework\Tests\Exceptions\Http;
+
+use Aphiria\Api\Errors\ProblemDetails;
+use Aphiria\Framework\Exceptions\Http\HttpExceptionRenderer;
+use Aphiria\Net\Http\IResponseFactory;
+use Aphiria\Net\Http\HttpStatusCodes;
+use Aphiria\Net\Http\IHttpRequestMessage;
+use Aphiria\Net\Http\IHttpResponseMessage;
+use Aphiria\Net\Http\IResponseWriter;
+use Aphiria\Net\Http\Response;
+use Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests the HTTP exception renderer
+ */
+class HttpExceptionRendererTest extends TestCase
+{
+    /** @var IResponseWriter|MockObject */
+    private IResponseWriter $responseWriter;
+    /** @var IHttpRequestMessage|MockObject */
+    private IHttpRequestMessage $request;
+    /** @var IResponseFactory|MockObject */
+    private IResponseFactory $responseFactory;
+
+    protected function setUp(): void
+    {
+        $this->request = $this->createMock(IHttpRequestMessage::class);
+        $this->responseFactory = $this->createMock(IResponseFactory::class);
+        $this->responseWriter = $this->createMock(IResponseWriter::class);
+    }
+
+    public function testHavingRequestSetButAnExceptionGetsThrownCausesGenericResponse(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(true, true, true);
+        $exceptionRenderer->registerResponseFactory(
+            Exception::class,
+            function (Exception $ex) {
+                throw new Exception();
+            }
+        );
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($this->callback(function (IHttpResponseMessage $response) {
+                return $response->getStatusCode() === HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR
+                    && $response->getBody() === null
+                    && $response->getHeaders()->count() === 0;
+            }));
+        $exceptionRenderer->render(new Exception);
+    }
+
+    public function testHavingRequestSetButNoResponseFactoryAndNotUsingProblemDetailsCreatesGenericResponse(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(false, true, true);
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($this->callback(function (IHttpResponseMessage $response) {
+                return $response->getStatusCode() === HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR
+                    && $response->getBody() === null
+                    && $response->getHeaders()->count() === 0;
+            }));
+        $exceptionRenderer->render(new Exception);
+    }
+
+    public function testHavingRequestSetButNoResponseFactoryAndUsingProblemDetailsCreatesProblemDetailsResponse(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(true, true, true);
+        $expectedResponse = new Response(HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR);
+        $this->responseFactory->expects($this->once())
+            ->method('createResponse')
+            ->with($this->request, HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR, null, $this->isInstanceOf(ProblemDetails::class))
+            ->willReturn($expectedResponse);
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($expectedResponse);
+        $exceptionRenderer->render(new Exception);
+    }
+
+    public function testHavingRequestSetWithAResponseFactoryCreatesResponseFromFactory(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(true, true, true);
+        $expectedResponse = new Response(HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR);
+        $exceptionRenderer->registerResponseFactory(
+            Exception::class,
+            fn (Exception $ex) => $expectedResponse
+        );
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($expectedResponse);
+        $exceptionRenderer->render(new Exception);
+    }
+
+    public function testHavingRequestSetWithManyResponseFactoriesCreatesResponseFromFactory(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(true, true, true);
+        $expectedResponse = new Response(HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR);
+        $exceptionRenderer->registerManyResponseFactories([
+            Exception::class => fn (Exception $ex) => $expectedResponse
+        ]);
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($expectedResponse);
+        $exceptionRenderer->render(new Exception);
+    }
+
+    public function testNotHavingRequestSetAndNotUsingProblemDetailsCreatesGenericResponse(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(false, false, false);
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($this->callback(function (IHttpResponseMessage $response) {
+                return $response->getStatusCode() === HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR
+                    && $response->getBody() === null
+                    && $response->getHeaders()->count() === 0;
+            }));
+        $exceptionRenderer->render(new Exception);
+    }
+
+    public function testNotHavingRequestSetAndUsingProblemDetailsCreatesProblemDetailsResponse(): void
+    {
+        $exceptionRenderer = $this->createExceptionRenderer(true, false, false);
+        $this->responseWriter->expects($this->once())
+            ->method('writeResponse')
+            ->with($this->callback(function (IHttpResponseMessage $response) {
+                return $response->getStatusCode() === HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR
+                    && $response->getBody() !== null
+                    && $response->getBody()->readAsString() === '{"type":"https:\/\/tools.ietf.org\/html\/rfc7231#section-6.6.1","title":"An error occurred","detail":null,"status":500,"instance":null}'
+                    && $response->getHeaders()->getFirst('Content-Type') === 'application/problem+json';
+            }));
+        $exceptionRenderer->render(new Exception);
+    }
+
+    /**
+     * Creates an exception renderer
+     *
+     * @param bool $useProblemDetails Whether or not to use problem details
+     * @param bool $setRequest Whether or not to set the request
+     * @param bool $setResponseFactory Whether or not to set the response factory
+     * @return HttpExceptionRenderer The exception renderer
+     */
+    private function createExceptionRenderer(
+        bool $useProblemDetails,
+        bool $setRequest,
+        bool $setResponseFactory
+    ): HttpExceptionRenderer {
+        return new HttpExceptionRenderer(
+            $useProblemDetails,
+            $setRequest ? $this->request : null,
+            $setResponseFactory ? $this->responseFactory : null,
+            $this->responseWriter
+        );
+    }
+}
